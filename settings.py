8187f0316d0cdd07618ef42db1f2ae8f692b6d08
shegsharem <logan72091@gmail.com>
2022-06-18 22:29:58 -0600

    fixed broken file save

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
+from re import S
+from telnetlib import SE
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import NS, messagebox
 from tkinter import simpledialog
 from tkinter import ttk, OptionMenu
 
@@ -7,7 +9,7 @@ from tkinter.font import BOLD
 from tkinter.ttk import Style
 
 import json
-from turtle import update
+from turtle import bgcolor, update
 
 
 
@@ -21,17 +23,21 @@ class Settings(tk.Frame):
         self.root = parent
 
         self.settingsFile = 'data/settings/gamesettings.json'
-        self.loadedFile = {}
-        self.loadedFileNonChanged = {}
+        #self.loadedFile = {}
+        #self.loadedFileNonChanged = {}
     
         Settings.openSettingsFile(self)
+        
+        
 
         self.GAME_RESOLUTION = tk.StringVar()
 
-        self.GAME_RESOLUTION.set(str(self.loadedFile['settings']['window_w'])+ ' x '+str(self.loadedFile['settings']['window_h']))
-        self.GAME_RESOLUTION.trace('w',None)
+        self.resolutionlist = [str(self.loadedFile['settings']['window_w'])+ 'x'+str(self.loadedFile['settings']['window_h']), 
+            '1980x1280', '840x680']
+        self.GAME_RESOLUTION.set(self.resolutionlist[0])
 
-        Settings.saveSettingsFile(self)
+        #self.GAME_RESOLUTION.set(str(self.loadedFile['settings']['window_w'])+ 'x'+str(self.loadedFile['settings']['window_h']))
+
 
         # Define Window Stuff
         self.root.title('Settings') # Set window title
@@ -40,8 +46,8 @@ class Settings(tk.Frame):
         self.root.iconbitmap('data/icons/settings.ico')
 
         # Define window dimensions
-        self.window_width = 800
-        self.window_height = 600
+        self.window_width = 500
+        self.window_height = 400
 
         # Get screen dimensions of computer
         self.screen_width = self.root.winfo_screenwidth()
@@ -55,64 +61,63 @@ class Settings(tk.Frame):
 
         self.root.attributes('-top',1) # Launch on top layer
 
+        self.root.columnconfigure(0, weight=0)
+        self.root.columnconfigure(1, weight=0)
+        self.root.columnconfigure(2, weight=5)
+
+        self.create_widgets()
         # ---------------------------------------- GUI OBJECTS -----------------------------------------------------------------
-        self.Title = ttk.Label(self.root, text='Settings',font=('Consolas', 24, BOLD),background="#FFFFFF",foreground="#000000")
+    def create_widgets(self):
+        parent = self.root
+        self.Title = ttk.Label(self.root, text='Settings',font=('', 12, BOLD),background="#FFFFFF",foreground="#000000")
+        self.Title.grid(column=0, row=0, sticky=tk.NW, padx=5, pady=5)
+
+
+
+        self.BLabel = ttk.Label(self.root, text=('Window Resolution'), font=('',11),background='#FFFFFF')
+    
+        self.resolution = ttk.Combobox(self.root, values=self.resolutionlist, textvariable=self.GAME_RESOLUTION)
+        self.BLabel.grid(column=0, row=1,sticky=tk.NS,padx=5,pady=5)
+        self.resolution.grid(column=1, row=1,sticky=tk.NS, padx=5,pady=0)
+        
+        self.ok_button = ttk.Button(self.root, text='Ok',command=self.on_closing)
+        self.ok_button.grid(column=2,row=5, sticky=tk.E, padx=0,pady=305)
+
+        self.cancel_button = ttk.Button(self.root, text='Cancel', command=self.root.destroy)
+        self.cancel_button.grid(column=3,row=5, sticky=tk.W, padx=10,pady=5)
+    
+        
         
 
-        self.menuRibbon = ttk.Notebook(parent, style='TNotebook')
-
-        
-        
-        self.menuRibbon.pack(pady=10, expand=True)
-        self.Title.pack(ipadx=10,ipady=10)
-        self.Title.place(relx=0.12,rely=0.05, anchor='center')
-
-        self.frame1 = ttk.Frame(self.menuRibbon, width=750, height=450)
-        self.frame2 = ttk.Frame(self.menuRibbon, width=750, height=450)
-
-        self.frame1.pack(fill='both', expand=True)
-
-        self.menuRibbon.add(self.frame1, text="General",)
-        self.menuRibbon.add(self.frame2, text="Graphics")
-
-        self.B = tk.Button(self.frame2, text='Change Window Width',
-            command=lambda : Settings.settingsNumChanger(self,'settings,window_w', 'Change Window Width'))
-        
-        self.BLabel = ttk.Label(self.frame2, textvariable= self.GAME_RESOLUTION,
-            font=('Consolas', 14, BOLD),background="#EEEEEE",foreground="#000000")
-        
-        self.B1 = tk.Button(self.frame2, text=('Change Window Height'),
-            command=lambda : Settings.settingsNumChanger(self,'settings,window_h', 'Change Window Height'))
-        
-        self.B.pack(ipadx=10,ipady=10, fill='both')
-        self.B1.pack(ipadx=10,ipady=10)
-        self.BLabel.pack(ipadx=10,ipady=10)
-
-        
-        
-        self.B.place(relx=0.2,rely=0.12,anchor='center')
-        self.B1.place(relx=0.2,rely=0.22, anchor='center')
-        self.BLabel.place(relx=0.2, rely=0.05, anchor='center')
+        self.resolution.bind('<<ComboboxSelected>>',lambda event: self.changeResolutionSetting())
+    
+    
 
         self.style = Style()
         self.style.configure('TNotebook', background='#FFFFFF')
        
 
     def on_closing(self):
-        if self.loadedFile == self.loadedFileNonChanged:
-            pass
-        else:
-            Settings.saveSettingsFile(self)
-            print("Saved Changes")
+        Settings.saveSettingsFile(self)
+        print("Saved Changes")
         self.root.destroy()
     
+    
+    def changeResolutionSetting(self, event=None):
+        self.selected = self.GAME_RESOLUTION.get()
+        print (self.selected)
+            #print(self.resolutionlist)
+        
+
+        dimensions = self.selected.split("x")
+        self.loadedFile['settings']['window_w'] = dimensions[0]
+        self.loadedFile['settings']['window_h'] = dimensions[1]
 
 
     def openSettingsFile(self):
         # Read json
         settingsFile = open(self.settingsFile, 'r')
         self.loadedFile = json.load(settingsFile)
-        self.loadedFileNonChanged = self.loadedFile
         settingsFile.close()
         
 
@@ -122,27 +127,6 @@ class Settings(tk.Frame):
         json.dump(self.loadedFile, settingsFile, indent=4)
         settingsFile.close()
         return print("Saved")
-
-           
-
-            
-
-
-    def settingsNumChanger(self, path, message):
-        Settings.openSettingsFile(self)
-
-        filepath = path.split(',')
-        self.originalnum = self.loadedFileNonChanged[filepath[0]][filepath[1]]
-
-        self.numInput = simpledialog.askinteger("Input", message, parent=self.root)
-        if self.numInput is not None:
-            self.loadedFile[filepath[0]][filepath[1]] = self.numInput.__str__()
-            if self.originalnum.__str__() == self.loadedFile[filepath[0]][filepath[1]]:
-                pass
-            else:
-                print("Changes made")
-                Settings.saveSettingsFile(self)
-                self.GAME_RESOLUTION.set(str(self.loadedFile['settings']['window_w'])+ ' x '+str(self.loadedFile['settings']['window_h']))
 
     
             
